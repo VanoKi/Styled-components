91f44e158fb8ff76a07a4138a1774bdc39c1eadd
VanoKi <kirsvano@gmail.com>
2025-02-28 00:44:31 +0300

    fix: add files to git

---
+++ src/components/StyledLink.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const StyledLink = styled.a`
+  border: none;
+  background-color: lightblue;
+  padding: 10px;
+  color: blue;
+  font-size: 2rem;
+  font-weight: bold;
+`
