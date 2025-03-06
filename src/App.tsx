9ccf4941995538673bc2a076b42e217fd164db3e
VanoKi <kirsvano@gmail.com>
2025-03-06 17:13:17 +0300

    feat: styled title subtitle and btn wrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,67 @@
-import { useState } from 'react'
 import './App.css'
 import styled from 'styled-components'
-import { StyledBnt} from './components/Button.styled'
-import { myTheme } from './styles/Theme.styled'
+import {myTheme} from './styles/Theme.styled'
 
 function App() {
-  // const [count, setCount] = useState(0)
+    // const [count, setCount] = useState(0)
 
-  return (
-    <div>
-        <Card>
-          <Img></Img>
-          <Title>Headline</Title>
-          <SubTitle>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</SubTitle>
-          <BtnWrap>
-            <Btn></Btn>
-            <Btn></Btn>
-          </BtnWrap>
-        </Card>
-    </div>
-  )
+    return (
+        <div>
+            <StyledCard>
+                <Img></Img>
+                <Title>Headline</Title>
+                <SubTitle>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</SubTitle>
+                <BtnWrap>
+                    <Btn></Btn>
+                    <Btn></Btn>
+                </BtnWrap>
+            </StyledCard>
+        </div>
+    )
 }
 
 export default App
 
-const Card = styled.div`
+const StyledCard = styled.div`
     margin: auto;
+    padding: 10px;
     width: 300px;
-    height: 300px;
+
     display: flex;
     flex-direction: column;
-    /* gap: 20px; */
     justify-content: center;
     align-items: center;
+    gap: 20px;
+
     background: #fff;
     box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
     border-radius: 15px;
 
     button {
-      cursor: pointer;
+        cursor: pointer;
     }
 
     @media ${myTheme.media.tablet} {
-      flex-direction: column;
+        flex-direction: column;
     }
+`
+const Img = styled.img``
+const Title = styled.h1`
+    font-weight: 700;
+    font-size: 16px;
+    color: #000;
+`
+const SubTitle = styled.p`
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 167%;
+    color: #abb3ba;
+`
+const BtnWrap = styled.div`
+    display: flex;
+    gap: 12px;
+`
+const Btn = styled.button`
+    width: 86px;
+    height: 30px;
 `
